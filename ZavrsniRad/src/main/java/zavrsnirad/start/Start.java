75cc6bf798deb29adf60f1f6773bce83e778fa20
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-02-12 17:09:22 +0100

    Creatrin a file Start
    Creating a file Start  for my project in Edunova School

---
+++ ZavrsniRad/src/main/java/zavrsnirad/start/Start.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zavrsnirad.start;
+
+import zavrsnirad.util.HibernateUtil;
+
+/**
+ *
+ * @author jbalo
+ */
+public class Start {
+    public static void main(String[] args) {
+        HibernateUtil.getSession();
+    }
+    
+}
