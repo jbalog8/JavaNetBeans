5e4ad2c80f4805c52dbab15b592a443df299e370
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-02-15 23:21:13 +0100

    Create a claas Olovka
    Creating a claas Olovka in Edunova School Lecture

---
+++ EdunovaJP25/src/main/java/edunova/model/vjezbanje/Olovka.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edunova.model.vjezbanje;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author CP
+ */
+@Entity(name = "olovke")
+public class Olovka {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(columnDefinition = "varchar(50)", nullable = false)
+    private String proizvodac;
+    
+    private Boolean ispravan;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProizvodac() {
+        return proizvodac;
+    }
+
+    public void setProizvodac(String proizvodac) {
+        this.proizvodac = proizvodac;
+    }
+
+    public Boolean getIspravan() {
+        return ispravan;
+    }
+
+    public void setIspravan(Boolean ispravan) {
+        this.ispravan = ispravan;
+    }
+    
+    
+    
+}
