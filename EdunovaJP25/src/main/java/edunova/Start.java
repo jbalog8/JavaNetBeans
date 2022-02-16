56c19fc894f818671a013602f04e631402589239
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-02-16 14:46:42 +0100

    Updating a class Start

--- EdunovaJP25/src/main/java/edunova/Start.java
+++ EdunovaJP25/src/main/java/edunova/Start.java
@@ -5,7 +5,11 @@
  */
 package edunova;
 
+import edunova.model.vjezbanje.veze.Mjesto;
+import edunova.model.vjezbanje.veze.Opcina;
 import edunova.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
 
 /**
  *
@@ -13,8 +17,61 @@ import edunova.util.HibernateUtil;
  */
 public class Start {
     
+    private Session session;
+
+    public Start() {
+        this.session = HibernateUtil.getSession();
+        
+        //primjerRadaSVezama();
+       procitajOpcine();
+    }
+    
+    private void procitajOpcine(){
+        // čitati https://docs.jboss.org/hibernate/core/3.3/reference/en/html/queryhql.html
+        // u dijelu upita Opcina tekst mora odgovarati imenu klase
+        List<Opcina> opcine = session.createQuery("from Opcina").list();
+        
+        for(Opcina o:opcine){
+            System.out.println(o.getNaziv());
+            o.getMjesta().forEach(m->System.out.println(m.getNaziv()));
+        }
+    }
+    
+    private void primjerRadaSVezama(){
+        session.beginTransaction();
+        Opcina opcina = new Opcina();
+        opcina.setNaziv("Bilje");
+        session.save(opcina);
+        Mjesto mjesto = new Mjesto();
+        mjesto.setNaziv("Kopačevo");
+        mjesto.setOpcina(opcina);
+        session.save(mjesto);
+        
+        mjesto = new Mjesto();
+        mjesto.setNaziv("Bilje");
+        mjesto.setOpcina(opcina);
+        session.save(mjesto);
+         session.getTransaction().commit();;
+    }
+    
+    
     public static void main(String[] args) {
-        HibernateUtil.getSession();
+    new Start();
+        /*
+        Session s = HibernateUtil.getSession();
+       
+        Mobitel m = new Mobitel();
+        m.setCijena(new BigDecimal(5999.99));
+        m.setDatumKupnje(new Date());
+        m.setNaziv("iPhone");
+        m.setIspravan(true);
+        m.setOpis("Mobitel je dobar");
+        m.setNapomena("Sve je OK");
+        
+        s.beginTransaction();
+        s.save(m);
+        s.getTransaction().commit();
+*/
     }
     
 }
