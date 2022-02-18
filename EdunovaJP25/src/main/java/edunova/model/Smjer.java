db498c3cb9988b2e02f131ec3d09d4fd7a19dc8c
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-02-18 21:46:36 +0100

    Creating class Smjer
    Creating class Smjer in Edunova School Lecture

---
+++ EdunovaJP25/src/main/java/edunova/model/Smjer.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edunova.model;
+
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author jbalog8
+ */
+@Entity
+public class Smjer extends Entitet{
+    
+    private String naziv;
+    private Integer trajanje;
+    private BigDecimal cijena;
+    private Boolean certificiran;
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public Integer getTrajanje() {
+        return trajanje;
+    }
+
+    public void setTrajanje(Integer trajanje) {
+        this.trajanje = trajanje;
+    }
+
+    public BigDecimal getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(BigDecimal cijena) {
+        this.cijena = cijena;
+    }
+
+    public Boolean getCertificiran() {
+        return certificiran;
+    }
+
+    public void setCertificiran(Boolean certificiran) {
+        this.certificiran = certificiran;
+    }
+    
+    
+}
