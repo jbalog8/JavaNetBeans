78b773f42331177d6665516a550f573a73bc0fdd
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-02-18 21:44:23 +0100

    Creating class Osoba
    Creating class Osoba in Edunova School Lecture

---
+++ EdunovaJP25/src/main/java/edunova/model/Osoba.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edunova.model;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+/**
+ *
+ * 
+ * @author jbalog8
+ */
+@MappedSuperclass
+public abstract class Osoba extends Entitet{
+    
+    private String ime;
+    private String prezime;
+    @Column(columnDefinition = "char(11)")
+    private String oib;
+    private String email;
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getOib() {
+        return oib;
+    }
+
+    public void setOib(String oib) {
+        this.oib = oib;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
