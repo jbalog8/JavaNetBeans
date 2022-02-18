14092d7176f06d148ec0baa1d0057fa7e48c1f18
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-02-18 21:45:09 +0100

    Creating class Grupa
    Creating class Grupa in Edunova School Lecture

---
+++ EdunovaJP25/src/main/java/edunova/model/Grupa.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edunova.model;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author jbalog8
+ */
+@Entity
+public class Grupa extends Entitet{
+    
+    private String naziv;
+    @ManyToOne
+    private Smjer smjer;
+    @ManyToOne
+    private Predavac predavac;
+    private Date datumPocetka;
+    @ManyToMany
+    private List<Polaznik> polaznici;
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public Predavac getPredavac() {
+        return predavac;
+    }
+
+    public void setPredavac(Predavac predavac) {
+        this.predavac = predavac;
+    }
+
+    public Date getDatumPocetka() {
+        return datumPocetka;
+    }
+
+    public void setDatumPocetka(Date datumPocetka) {
+        this.datumPocetka = datumPocetka;
+    }
+
+    public List<Polaznik> getPolaznici() {
+        return polaznici;
+    }
+
+    public void setPolaznici(List<Polaznik> polaznici) {
+        this.polaznici = polaznici;
+    }
+
+    public Smjer getSmjer() {
+        return smjer;
+    }
+
+    public void setSmjer(Smjer smjer) {
+        this.smjer = smjer;
+    }
+    
+    
+    
+}
