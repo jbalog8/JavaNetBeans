d479ed9abc19e663414c1219b2ef227300e51e9a
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-02-18 21:45:46 +0100

    Creating class Polaznik
    Creating class Polaznik in Edunova School Lecture

---
+++ EdunovaJP25/src/main/java/edunova/model/Polaznik.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edunova.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author jbalog8
+ */
+@Entity
+public class Polaznik extends Osoba{
+    
+     private String brojUgovora;
+
+    public String getBrojUgovora() {
+        return brojUgovora;
+    }
+
+    public void setBrojUgovora(String brojUgovora) {
+        this.brojUgovora = brojUgovora;
+    }
+     
+     
+}
