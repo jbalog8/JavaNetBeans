28f5054d3107c62ff05b28b3c3387f73b46be7d1
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-02-22 21:56:49 +0100

    Creating class Edunova Util
    Creating class Edunova Util in Edunova School Lecture

---
+++ EdunovaJP25/src/main/java/edunova/util/EdunovaUtil.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edunova.util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import us.codecraft.xsoup.Xsoup;
+
+/**
+ *
+ * @author jbalog8
+ */
+public class EdunovaUtil {
+    public static String generirajOib(){
+         try {
+             URL url = new URL("http://oib.itcentrala.com/oib-generator/");
+             BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            url.openStream()));
+            String inputLine;
+            StringBuilder sb = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                sb.append(inputLine);
+            }
+            in.close();
+             Document d = Jsoup.parse(sb.toString());
+            return Xsoup.compile("/html/body/div[1]/div[1]/text()").evaluate(d).get();
+        } catch (Exception e) {
+        }
+        return "";
+    }
+}
