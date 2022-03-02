077198c562affd514d4c00b29a04d0eefb23300f
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-03-02 23:36:02 +0100

    Creating class Operater
    Creating class Operater in Edunova School  Lecture

---
+++ EdunovaJP25/src/main/java/edunova/model/Operater.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edunova.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author jbalog8
+ */
+@Entity
+public class Operater extends Osoba {
+
+    private String lozinka;
+    private String uloga;
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+
+    public String getUloga() {
+        return uloga;
+    }
+
+    public void setUloga(String uloga) {
+        this.uloga = uloga;
+    }
+
+}
