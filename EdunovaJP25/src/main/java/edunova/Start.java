ad0b425cd7351a6a2e96ca37df7053a5817b0868
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-03-02 23:37:36 +0100

    Updating Start

--- EdunovaJP25/src/main/java/edunova/Start.java
+++ EdunovaJP25/src/main/java/edunova/Start.java
@@ -64,6 +64,7 @@ public class Start {
     public static void main(String[] args) {
        // new Start();
         new SplashScreen().setVisible(true);
+       // PocetniInsert.unosOperatera();
         
         
         /*
