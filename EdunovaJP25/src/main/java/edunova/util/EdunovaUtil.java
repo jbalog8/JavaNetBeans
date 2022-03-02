0d047545ce3b3c11058b127d3699a24c5b0efb6c
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-03-02 23:36:51 +0100

    Updating Edunova Util

--- EdunovaJP25/src/main/java/edunova/util/EdunovaUtil.java
+++ EdunovaJP25/src/main/java/edunova/util/EdunovaUtil.java
@@ -4,6 +4,7 @@
  */
 package edunova.util;
 
+import edunova.model.Operater;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -16,10 +17,22 @@ import us.codecraft.xsoup.Xsoup;
  * @author jbalog8
  */
 public class EdunovaUtil {
-    public static String generirajOib(){
-         try {
-             URL url = new URL("http://oib.itcentrala.com/oib-generator/");
-             BufferedReader in = new BufferedReader(
+
+    public static Operater operater;
+    public static final String NAZIV_APP = "Edunova app";
+
+    public static String getNaslov(String naslov) {
+        if (EdunovaUtil.operater == null) {
+            return EdunovaUtil.NAZIV_APP + " " + naslov;
+        }
+        return EdunovaUtil.NAZIV_APP + " " + naslov + " " + EdunovaUtil.operater.getIme() + " "
+                + EdunovaUtil.operater.getPrezime() + " - " + EdunovaUtil.operater.getUloga();
+    }
+
+    public static String generirajOib() {
+        try {
+            URL url = new URL("http://oib.itcentrala.com/oib-generator/");
+            BufferedReader in = new BufferedReader(
                     new InputStreamReader(
                             url.openStream()));
             String inputLine;
@@ -28,7 +41,7 @@ public class EdunovaUtil {
                 sb.append(inputLine);
             }
             in.close();
-             Document d = Jsoup.parse(sb.toString());
+            Document d = Jsoup.parse(sb.toString());
             return Xsoup.compile("/html/body/div[1]/div[1]/text()").evaluate(d).get();
         } catch (Exception e) {
         }
