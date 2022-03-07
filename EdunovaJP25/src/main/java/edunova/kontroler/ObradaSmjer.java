c2fba390b16ce3349040c4eb52e8a9a9c954c696
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-03-07 23:37:40 +0100

    Update Obrada Smjer

--- EdunovaJP25/src/main/java/edunova/kontroler/ObradaSmjer.java
+++ EdunovaJP25/src/main/java/edunova/kontroler/ObradaSmjer.java
@@ -4,8 +4,10 @@
  */
 package edunova.kontroler;
 
+import edunova.model.Grupe;
 import edunova.model.Smjer;
 import edunova.util.EdunovaException;
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -16,23 +18,37 @@ public class ObradaSmjer extends Obrada<Smjer>{
 
     @Override
     public List<Smjer> read() {
-        return session.createQuery("from Smjer").list();
+        return session.createQuery("from Smjer a order by a.naziv").list();
      }
 
     @Override
     protected void kontorolaCreate() throws EdunovaException {
         kontrolaNaziv();
         kontrolaTrajanje();
+        kontrolaCijena();
     }
 
     @Override
     protected void kontorlaUpdate() throws EdunovaException {
-        
+         kontrolaNaziv();
+        kontrolaTrajanje();
+        kontrolaCijena();
     }
 
     @Override
     protected void kontrolaDelete() throws EdunovaException {
        
+          if(entitet.getGrupe()!=null && entitet.getGrupe().size()>0){
+            
+            StringBuilder sb = new StringBuilder();
+            sb.append("\n");
+            for(Grupe g:entitet.getGrupe()){
+                sb.append(g.getNaziv());
+                sb.append("\n");
+            }
+            
+            throw new EdunovaException("Ne možete brisati smjer jer na njemu ima grupa" + sb.toString());
+        }
     }
     
     
@@ -52,6 +68,14 @@ public class ObradaSmjer extends Obrada<Smjer>{
         
         if(entitet.getTrajanje() == null || entitet.getTrajanje() < 0){
             throw new EdunovaException ("Trajanje mora biti postavljeno i ne smije biti manje od 0");
+        }
+    }
+
+    private void kontrolaCijena() throws EdunovaException{
+       if( entitet.getCijena()==null || 
+                entitet.getCijena().compareTo(BigDecimal.ZERO.ZERO)<0 ||
+                entitet.getCijena().compareTo(new BigDecimal(10000))>0){
+            throw new EdunovaException("Cijena mora biti postavljena, veća od 0 i manja od 10000");
         }
     }
 }
