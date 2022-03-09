bfc5685b25bb8479c43e3e57fa7b62534ed18508
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-03-09 23:28:23 +0100

    Creating class Prikaz Polaznik
    Creating class Prikaz Polaznik in Edunova School Lecture

---
+++ EdunovaJP25/src/main/java/edunova/view/PrikazPolaznika.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edunova.view;
+
+import edunova.model.Polaznik;
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+/**
+ *
+ * @author jbalog8
+ */
+public class PrikazPolaznika extends JLabel implements ListCellRenderer<Polaznik>{
+    
+    public PrikazPolaznika(){
+        
+        setOpaque(true);
+}
+
+  
+
+    @Override
+    public Component getListCellRendererComponent(JList<? extends Polaznik> list, Polaznik value, int index, boolean isSelected, boolean cellHasFocus) {
+        if (isSelected) {
+            setBackground(Color.BLUE);
+            setForeground(Color.WHITE);
+        } else {
+            setBackground(Color.WHITE);
+            setForeground(Color.BLACK);
+        }
+        
+        setText((value.getIme()==null ? "Nepoznato" : value.getIme())
+                + " " + (value.getPrezime()==null ? "Nepoznato" : value.getPrezime()));
+
+        return this;
+    }
+    
+}
