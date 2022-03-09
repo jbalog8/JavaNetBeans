0beaa4c43f95513d97e4ebe41a8ba3c8d7f18fbe
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-03-10 00:22:13 +0100

    Creating class Prikaz Osoba
    Creating class Prikaz Osoba in Edunova School Lecture

---
+++ EdunovaJP25/src/main/java/edunova/view/PrikazOsoba.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edunova.view;
+
+import edunova.model.Osoba;
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+/**
+ *
+ * @author jbalog8
+ */
+
+public class PrikazOsoba extends JLabel implements ListCellRenderer<Osoba> {
+
+    public PrikazOsoba() {
+
+        setOpaque(true);
+    }
+
+    @Override
+    public Component getListCellRendererComponent(JList<? extends Osoba> list, Osoba value, int index, boolean isSelected, boolean cellHasFocus) {
+        if (isSelected) {
+            setBackground(Color.BLUE);
+            setForeground(Color.WHITE);
+        } else {
+            setBackground(Color.WHITE);
+            setForeground(Color.BLACK);
+        }
+
+        setText((value.getIme() == null ? "Nepoznato" : value.getIme())
+                + " " + (value.getPrezime() == null ? "Nepoznato" : value.getPrezime()));
+
+        return this;
+    }
+
+}
