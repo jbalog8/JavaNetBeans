7fb2ea357af04f173a3c4860de92323a351c62f1
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-03-23 23:34:46 +0100

    Updating class Prikaz Polazika

--- EdunovaJP25/src/main/java/edunova/view/PrikazPolaznika.java
+++ EdunovaJP25/src/main/java/edunova/view/PrikazPolaznika.java
@@ -7,6 +7,8 @@ package edunova.view;
 import edunova.model.Polaznik;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
@@ -15,30 +17,42 @@ import javax.swing.ListCellRenderer;
  *
  * @author jbalog8
  */
-@Deprecated(since = "Prebacio na PrikazOsoba")
-public class PrikazPolaznika extends JLabel implements ListCellRenderer<Polaznik>{
-    
-    public PrikazPolaznika(){
-        
-        setOpaque(true);
-}
+public class PrikazPolaznika extends JLabel implements ListCellRenderer<Polaznik> {
 
-  
+    private String ime;
+    private DefaultListCellRenderer defaultRenderer = new DefaultListCellRenderer();
+    private int maksimalnoZnakovaIme = 12;
+
+    public PrikazPolaznika() {
+
+        setOpaque(true);
+    }
 
     @Override
     public Component getListCellRendererComponent(JList<? extends Polaznik> list, Polaznik value, int index, boolean isSelected, boolean cellHasFocus) {
-        if (isSelected) {
-            setBackground(Color.BLUE);
-            setForeground(Color.WHITE);
-        } else {
-            setBackground(Color.WHITE);
-            setForeground(Color.BLACK);
-        }
-        
-        setText((value.getIme()==null ? "Nepoznato" : value.getIme())
-                + " " + (value.getPrezime()==null ? "Nepoznato" : value.getPrezime()));
+       JLabel renderer = (JLabel) defaultRenderer.getListCellRendererComponent(list, value, index,
+                isSelected, cellHasFocus);
 
-        return this;
+        renderer.setFont(new Font("Courier new", Font.BOLD, 12));
+
+        if (isSelected) {
+            renderer.setBackground(Color.BLUE);
+            renderer.setForeground(Color.WHITE);
+        } else {
+            renderer.setBackground(Color.WHITE);
+            renderer.setForeground(Color.BLACK);
+
+        }
+        ime = value.getIme();
+        for (int i = 0; i < maksimalnoZnakovaIme; i++) {
+            ime += " ";
+        }
+
+        ime = ime.substring(0, maksimalnoZnakovaIme);
+
+        renderer.setText(ime + value.getPrezime());
+
+        return renderer;
     }
-    
+
 }
